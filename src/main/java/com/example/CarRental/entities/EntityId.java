dc026eae2dfe4b2421bef836b856915044e6a787
“Nastya” <nastya.cap04@gmail.com>
2024-07-10 21:32:25 +0300

    add EntityId

---
+++ src/main/java/com/example/CarRental/entities/EntityId.java
@@ -0,0 +1,19 @@
+package com.example.CarRental.entities;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+@Inheritance(strategy= InheritanceType.JOINED)
+public abstract class EntityId{
+    private long id;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Id
+    @Column(name = "id")
+    public long getId() {
+        return id;
+    }
+}
